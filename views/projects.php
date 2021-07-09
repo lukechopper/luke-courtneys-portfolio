500df516c821d84357285362d9de60edb0c11160
lukechopper <lukechopper22@gmail.com>
2021-07-09 15:38:11 +0100

    Finished refactoring projects view

--- views/projects.php
+++ views/projects.php
@@ -1,4 +1,4 @@
-<?php include_once '../data/projectsData.php'; ?>
+<?php include_once './data/projectsData.php'; ?>
 
 <div class="project-background">
 <section class="main-container main-container--project-container">
@@ -18,123 +18,23 @@
     </div>
     <!--BEGIN GALLERY SECTION-->
     <div class="project-section__gallery-container">
-        <div class="project-section__gallery-container-item" type="react,frontend" id="firstItem">
+        <?php 
+        foreach ($projectsData as $galleryContainer){
+            echo '<div class="project-section__gallery-container-item" type="'.$galleryContainer['type'].'" id="'.$galleryContainer['id'].'">
             <section class="project-section__gallery-container-item-section">
             <div class="project-section__gallery-container-item-content">
                 <div class="project-section__gallery-container-gallery-header">
-                    <h1 class="project-section__gallery-container-item-header">La Stalla At Stables</h1>
-                    <p class="project-section__gallery-container-item-paragraph" >React / Next JS</p>
+                    <h1 class="project-section__gallery-container-item-header">'.$galleryContainer['title'].'</h1>
+                    <p class="project-section__gallery-container-item-paragraph" >'.$galleryContainer['tech'].'</p>
                 </div>
                 <div class="project-section__gallery-container-btn">learn more</div>
             </div>
-            <img class="project-section__gallery-container-img" src="https://i2.lensdump.com/i/Zg3aqZ.jpg" alt="Zg3aqZ.jpg" />
+            <img class="project-section__gallery-container-img" src="'.$galleryContainer['img'].'" alt="Zg3aqZ.jpg" />
             <div class="project-section__gallery-container-overlay"></div>
             </section>
-        </div>
-        <div class="project-section__gallery-container-item" type="frontend" id="secondItem">
-            <section class="project-section__gallery-container-item-section">
-            <div class="project-section__gallery-container-item-content">
-                <div class="project-section__gallery-container-gallery-header">
-                    <h1 class="project-section__gallery-container-item-header" >Tank Battle Game</h1>
-                    <p class="project-section__gallery-container-item-paragraph">JavaScript / HTML Canvas</p>
-                </div>
-                <div class="project-section__gallery-container-btn">learn more</div>
-            </div>
-            <img class="project-section__gallery-container-img" src="https://i1.lensdump.com/i/ZBAVBT.png" alt="Zg3hMP.png"/>
-            <div class="project-section__gallery-container-overlay"></div>
-        </section>
-        </div>
-        <div class="project-section__gallery-container-item" type="react,backend" id="thirdItem">
-            <section class="project-section__gallery-container-item-section">
-            <div class="project-section__gallery-container-item-content">
-                <div class="project-section__gallery-container-gallery-header">
-                    <h1 class="project-section__gallery-container-item-header" >Ersatz Amazon Sidebar</h1>
-                    <p class="project-section__gallery-container-item-paragraph" >React / Nodejs</p>
-                </div>
-                <div class="project-section__gallery-container-btn">learn more</div>
-            </div>
-            <img class="project-section__gallery-container-img" src="https://i3.lensdump.com/i/ZBAq27.png" alt="Zg41YT.jpg"  />
-            <div class="project-section__gallery-container-overlay"></div>
-        </section>
-        </div>
-        <div class="project-section__gallery-container-item" type="frontend" id="fourthItem">
-            <section class="project-section__gallery-container-item-section">
-            <div class="project-section__gallery-container-item-content">
-                <div class="project-section__gallery-container-gallery-header">
-                    <h1 class="project-section__gallery-container-item-header" >Ersatz BBC Website</h1>
-                    <p class="project-section__gallery-container-item-paragraph" >HTML, CSS & JavaScript</p>
-                </div>
-                <div class="project-section__gallery-container-btn">learn more</div>
-            </div>
-            <img class="project-section__gallery-container-img" src="https://i.lensdump.com/i/ZBAloQ.png" alt="Zg4ACi.jpg" />
-            <div class="project-section__gallery-container-overlay"></div>
-        </section>
-        </div>
-        <div class="project-section__gallery-container-item" type="frontend" id="fifthItem">
-            <section class="project-section__gallery-container-item-section">
-            <div class="project-section__gallery-container-item-content">
-                <div class="project-section__gallery-container-gallery-header">
-                    <h1 class="project-section__gallery-container-item-header" >Circus Game</h1>
-                    <p class="project-section__gallery-container-item-paragraph" >JavaScript / HTML Canvas</p>
-                </div>
-                <div class="project-section__gallery-container-btn">learn more</div>
-            </div>
-            <img class="project-section__gallery-container-img" src="https://i3.lensdump.com/i/ZBAnek.png" alt="Zg4Wzo.jpg" />
-            <div class="project-section__gallery-container-overlay"></div>
-        </section>
-        </div>
-        <div class="project-section__gallery-container-item" type="react,frontend" id="sixthItem">
-            <section class="project-section__gallery-container-item-section">
-            <div class="project-section__gallery-container-item-content">
-                <div class="project-section__gallery-container-gallery-header">
-                    <h1 class="project-section__gallery-container-item-header" >Live Extreme Weather Tracker</h1>
-                    <p class="project-section__gallery-container-item-paragraph" >React</p>
-                </div>
-                <div class="project-section__gallery-container-btn">learn more</div>
-            </div>
-            <img class="project-section__gallery-container-img" src="https://i.lensdump.com/i/ZBAvxx.png" alt="Zg4iw9.jpg" />
-            <div class="project-section__gallery-container-overlay"></div>
-        </section>
-        </div>
-        <div class="project-section__gallery-container-item" type="react,backend" id="seventhItem">
-            <section class="project-section__gallery-container-item-section">
-            <div class="project-section__gallery-container-item-content">
-                <div class="project-section__gallery-container-gallery-header">
-                    <h1 class="project-section__gallery-container-item-header" >YouTube Style Comments System</h1>
-                    <p class="project-section__gallery-container-item-paragraph" >React / Nodejs</p>
-                </div>
-                <div class="project-section__gallery-container-btn">learn more</div>
-            </div>
-            <img class="project-section__gallery-container-img" src="https://i1.lensdump.com/i/ZBAMRH.png" alt="ZgKtUH.jpg" />
-            <div class="project-section__gallery-container-overlay"></div>
-        </section>
-        </div>
-        <div class="project-section__gallery-container-item" type="backend" id="eigthItem">
-            <section class="project-section__gallery-container-item-section">
-            <div class="project-section__gallery-container-item-content">
-                <div class="project-section__gallery-container-gallery-header">
-                    <h1 class="project-section__gallery-container-item-header" >Typing Test Game</h1>
-                    <p class="project-section__gallery-container-item-paragraph" >Nodejs / HTML, CSS & JavaScript</p>
-                </div>
-                <div class="project-section__gallery-container-btn">learn more</div>
-            </div>
-            <img class="project-section__gallery-container-img" src="https://i2.lensdump.com/i/ZBAY71.png" alt="Zg4IKv.jpg"  />
-            <div class="project-section__gallery-container-overlay"></div>
-        </section>
-        </div>
-        <div class="project-section__gallery-container-item" type="react,frontend" id="ninthItem">
-            <section class="project-section__gallery-container-item-section">
-            <div class="project-section__gallery-container-item-content">
-                <div class="project-section__gallery-container-gallery-header">
-                    <h1 class="project-section__gallery-container-item-header" >Preference Finder Interactive Quiz</h1>
-                    <p class="project-section__gallery-container-item-paragraph" >React / Framer Motion</p>
-                </div>
-                <div class="project-section__gallery-container-btn">learn more</div>
-            </div>
-            <img class="project-section__gallery-container-img" src="https://i2.lensdump.com/i/ZBW4vi.png" alt="Zg4tqz.jpg" />
-            <div class="project-section__gallery-container-overlay"></div>
-        </section>
-        </div>
+        </div>';
+        }
+        ?>
     </div>
 </section>
 </section>
